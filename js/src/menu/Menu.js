48f98452802fdea28399dd78df0f39408867afc7
MLuschikov <120097914+MLuschikov@users.noreply.github.com>
2022-12-24 00:56:49 +0400

    Delete js directory

--- js/src/menu/Menu.js
+++
@@ -1,5 +0,0 @@
-class Menu extends Component {
-    addEventListeners() {
-        document.querySelectorAll('.menu-item').forEach(button => button.addEventListener('click', (event) => this.callbacks.showMenuItem(event.target.dataset.item)));
-    }
-}
--- js/src/menu/MenuTemplate.js
+++
@@ -1,6 +0,0 @@
-Template.prototype.Menu = () =>
-    <button data-item: "component1" > Нажми1
-    </button >
-
-    <button data-item: "component2" > Нажми2
-    </button >
